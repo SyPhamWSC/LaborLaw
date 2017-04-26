0030b9c49a4258bafc15154525dac1ae35c53fdf
SyPhamWSC <sy.phamtiennd@gmail.com>
2017-04-26 14:01:55 +0700

    Cofig something

---
+++ app/src/main/java/com/dtsgroup/labourlaw/fragment/EnterGuideFragment.java
@@ -0,0 +1,21 @@
+package com.dtsgroup.labourlaw.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dtsgroup.labourlaw.R;
+
+
+public class EnterGuideFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_enter_guide, container, false);
+        return view;
+    }
+}
